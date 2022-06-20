690f511e3751670f21aa2c8455559a34c1a83c18
marurevi <maryrev@hotmail.com>
2022-06-20 14:40:07 -0300

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Navigator />
+      <Routes>
+        <Route index element={Home} />
+        <Route path="/details" element={Details} />
+      </Routes>
       CAPSTONE
     </div>
   );
