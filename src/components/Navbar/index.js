48a0e583ca4c381365f0d317e028ef2dbc167c48
marurevi <maryrev@hotmail.com>
2022-06-20 19:07:07 -0300

    Fix linters

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {
-  Nav, NavLink, Bars, NavMenu, Close, PopUpNav
+  Nav, NavLink, Bars, NavMenu, Close, PopUpNav,
 } from './NavbarStyle';
 
 function Navbar() {
@@ -8,22 +8,24 @@ function Navbar() {
   return (
     <div>
       <Nav>
-        {popUp ? 
-        <>
-          <Close onClick={() => setPopUp(false)} />
-          <PopUpNav>
-            <NavLink to="/" onClick={() => setPopUp(false)}>
-              Home
-            </NavLink>
-            <NavLink to="/details" activestyle="true" onClick={() => setPopUp(false)}>
-              Details
-            </NavLink>
-            <NavLink to="/contact-us" activestyle="true" onClick={() => setPopUp(false)}>
-              Contact us
-            </NavLink>
-          </PopUpNav>
-        </> 
-        : <Bars onClick={() => setPopUp(true)} />}
+        {popUp
+          ? (
+            <>
+              <Close onClick={() => setPopUp(false)} />
+              <PopUpNav>
+                <NavLink to="/" onClick={() => setPopUp(false)}>
+                  Home
+                </NavLink>
+                <NavLink to="/details" activestyle="true" onClick={() => setPopUp(false)}>
+                  Details
+                </NavLink>
+                <NavLink to="/contact-us" activestyle="true" onClick={() => setPopUp(false)}>
+                  Contact us
+                </NavLink>
+              </PopUpNav>
+            </>
+          )
+          : <Bars onClick={() => setPopUp(true)} />}
         <NavMenu>
           <NavLink to="/">
             Home
