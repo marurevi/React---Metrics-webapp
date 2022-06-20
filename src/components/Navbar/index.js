4539bcc955f8814a405614ebe66abba406182dc8
marurevi <maryrev@hotmail.com>
2022-06-20 19:06:01 -0300

    Add links to mobile menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Nav, NavLink, Bars, NavMenu,
+  Nav, NavLink, Bars, NavMenu, Close, PopUpNav
 } from './NavbarStyle';
 
 function Navbar() {
+  const [popUp, setPopUp] = useState(false);
   return (
     <div>
       <Nav>
-        <Bars />
+        {popUp ? 
+        <>
+          <Close onClick={() => setPopUp(false)} />
+          <PopUpNav>
+            <NavLink to="/" onClick={() => setPopUp(false)}>
+              Home
+            </NavLink>
+            <NavLink to="/details" activestyle="true" onClick={() => setPopUp(false)}>
+              Details
+            </NavLink>
+            <NavLink to="/contact-us" activestyle="true" onClick={() => setPopUp(false)}>
+              Contact us
+            </NavLink>
+          </PopUpNav>
+        </> 
+        : <Bars onClick={() => setPopUp(true)} />}
         <NavMenu>
           <NavLink to="/">
             Home
           </NavLink>
-          <NavLink to="/details" activeStyle>
+          <NavLink to="/details" activestyle="true">
             Details
           </NavLink>
-          <NavLink to="/contact-us" activeStyle>
+          <NavLink to="/contact-us" activestyle="true">
             Contact us
           </NavLink>
         </NavMenu>
-
       </Nav>
     </div>
   );
